0ded9dd61af06ef1d9daeb99b638cf30fb5941da
Tanuaaz <73118470+Tanuaaz@users.noreply.github.com>
2021-03-22 04:52:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+'''Данная программа позволяет проверить корректность введенного номера ИНН или ОГРН
+и вывести соотвествующую ему информацию в случае правильно введенного номера'''
 def check_ogrn(ogrn):
-    '''Проверка правильности введенного номера'''
+    '''Проверка правильности введенного номера ОГРН'''
     return len(ogrn) == 13 and (int(ogrn[:-1]) % 11) % 10 == int(ogrn[-1])
 
 
@@ -28,10 +30,60 @@ def print_info(ogrn):
     ogrn5 = ogrn[12]
     print("Контрольное число: " + ogrn5)
 
+def inn_ctrl_summ(inn):
+    ''' Подсчет контрольной суммы '''
+    n1 = [7, 2, 4, 10, 3, 5, 9, 4, 6, 8]
+    n2 = [3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8]
+    n_1_summ = 0
+    n_2_summ = 0
+    for i in range(0, len(n1)):
+        n_1_summ += int (inn[i]) * int (n1[i])
+    for i in range(0, len(n2)):
+        n_2_summ += int (inn[i]) * int (n2[i])
+    cntl_summ_n1 = n_1_summ % 11 % 10
+    cntl_summ_n2 = n_2_summ % 11 % 10
+    return cntl_summ_n1, cntl_summ_n2
 
-print("Введите ОГРН: ", end='')
-ogrn = input()
-while not check_ogrn(ogrn):
-    print("Введён неверный ОГРН, повторите попытку: ", end='')
-    ogrn = input()
-print_info(ogrn)
+def inn_check(inn):
+    '''Проверка правильности введенного номера ИНН'''
+    cntl_summ_n1, cntl_summ_n2 = inn_ctrl_summ(inn)
+    return cntl_summ_n1 == int (inn[-2]) and cntl_summ_n2 == int (inn[-1])
+
+def print_inn(inn):
+    '''Вывод информации о предприятии по его ИНН'''
+    inn1 = inn[0:2]
+    print("Код субъекта РФ: " + inn1)
+
+    inn2 = inn[2:4]
+    print("Номер местной налоговой инспекции: " + inn2)
+
+    inn3 = inn[5:10]
+    print("Номер налоговой записи налогоплательщика в территориальном разделе: " + inn3)
+
+    inn4 = inn[-1]
+    print("Контрольное число 1: " + inn4)
+
+    inn5 = inn[-2]
+    print("Контрольное число 2: " + inn5)
+
+def inn_or_ogrn ():
+    '''Выбор типа проверки в зависимости от вида цифрового кода'''
+    check = input ('Введите тип цифрового кода: ИНН или ОГРН ')
+    check.upper()
+    if check == 'ИНН':
+        print("Введите ИНН: ", end='')
+        inn = input()
+        inn_ctrl_summ(inn)
+        while inn_check(inn) == False:
+            print("Введён неверный ИНН, повторите попытку: ", end='')
+            inn = input()
+        print_inn(inn)
+    else:
+        print("Введите ОГРН: ", end='')
+        ogrn = input()
+        while not check_ogrn(ogrn):
+            print("Введён неверный ОГРН, повторите попытку: ", end='')
+            ogrn = input()
+        print_info(ogrn)
+
+inn_or_ogrn ()
